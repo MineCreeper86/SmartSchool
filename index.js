82ab8d445acf42986651787e394ae62a4100430b
MineCreeper86 <61276297+MineCreeper86@users.noreply.github.com>
2021-10-01 12:20:24 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $("div.navbar").mouseover(function(){
+    $("div.navbar").css("background-color","#C6F4E5");
+  });
+  $("div.navbar").mouseout(function(){
+    $("div.navbar").css("background-color","#D3FFF1");
+  });
+});
